41419f84992802d0d3b8f30b93987a407336de63
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-07 14:35:12 +0300

    jkk

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var config = require('config');
+var config = require('./config.json');
 
 var indexRouter = require('./routes/index');
 
