ef129663331bcb9f633789fcf7304af9eb4914f9
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-07 17:01:18 +0300

    jkk

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  let translations=await req.knex("t_translations").orderBy("id")
-  res.render('index', { translations });
+  res.render('index', { });
 });
 router.get('/admin', async function(req, res, next) {
-  res.render('admin', { title: 'Express' });
+  let translations=await req.knex("t_translations").orderBy("id")
+  res.render('admin', {  translations});
 });
 router.post('/admin/AddSession', async function(req, res, next) {
    await req.knex("t_translations").insert({},"*")
   res.redirect('/spief2023/admin');
 });
+
 
 
 module.exports = router;
