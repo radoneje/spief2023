d126ba9300968082f1d0ef64937a26a077d0fb00
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-10 22:02:10 +0300

    jkk

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use('/', (req, res,next)=>{
   req.knex=knex;
   req.messageToBot=async (text)=>{
     for(let c of chats){
-      await bot.sendMessage(c, text, {parse_mode: 'HTML', link_preview:false})
+      await bot.sendMessage(c, text, {parse_mode: 'HTML', disable_web_page_preview:false})
     }
   }
   next();
