281420f581012836893a1cd803816ada63d35e37
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-10 23:23:46 +0300

    jkk

--- routes/index.js
+++ routes/index.js
@@ -297,9 +297,9 @@ router.get('/showFiles/:trid/:lang', async function(req, res, next) {
 router.get('/setTrRec/:trid/:lang/:file', async function(req, res, next) {
     try {
         let dt={}
-        dt["rec_"+req.params.land]=req.params.file
+        dt["rec_"+req.params.lang]=req.params.file
         let r =await req.knex("t_translations").update(dt, "*").where({id:req.params.trid})
-        res.render("trRecord", {file:r[0]["rec_"+req.params.land]})
+        res.render("trRecord", {file:r[0]["rec_"+req.params.lang]})
     }
     catch (e){
         console.warn(e)
