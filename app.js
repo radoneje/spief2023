e0358a15e123cc6c3146057015bd05ce9f0828f5
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-10 14:04:23 +0300

    jkk

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const knex = require('knex')({
   searchPath: ['knex', 'public'],
 });
 
+/*const TelegramBot = require('node-telegram-bot-api');
+const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const bot = new TelegramBot(token, {polling: true});
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});*/
+
 
 var app = express();
 
@@ -27,6 +36,7 @@ app.use(cookieParser());
 app.use("/spief2023",express.static(path.join(__dirname, 'public')));
 app.use('/', (req, res,next)=>{
   req.knex=knex;
+ // req.bot=bot;
   next();
 });
 app.use('/', indexRouter);
