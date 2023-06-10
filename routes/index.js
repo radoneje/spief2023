8e20f3c0c158c68138ec2977506d7b4e48fcb4d7
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-10 22:33:44 +0300

    jkk

--- routes/index.js
+++ routes/index.js
@@ -286,7 +286,7 @@ router.get('/trSbertvExcel', async function(req, res, next) {
 
 router.get('/showFiles/:trid/:lang', async function(req, res, next) {
     try {
-        let files =await req.knex("t_records").where({trid:req.params.id,lang:req.params.lang })
+        let files =await req.knex("t_records").where({trid:req.params.trid,lang:req.params.lang })
             res.render("showFiles", {files})
     }
     catch (e){
