94df87846568a0177dc7ff08c4687f0248dcc886
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-10 21:29:13 +0300

    jkk

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ const token = '6067969380:AAHognfYT5RhESuaK4N6RSfj26EuB8nBDis';
 const bot = new TelegramBot(token, {polling: true});
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  console.warn(msg.chat.id)
   // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message\n', chatId);
+  //bot.sendMessage(chatId, 'Received your message\n', chatId);
 });
 
 
