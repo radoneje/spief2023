bb5637cf11755778ad04571b221636d88fc54576
radoneje <53825282+radoneje@users.noreply.github.com>
2023-06-14 15:47:42 +0300

    jkk

--- cutter.js
+++ cutter.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 
 var path = require('path');
 var config = require('./config.json');
+const {spawn} = require('child_process');
 
 var indexRouter = require('./routes/index');
 
@@ -14,30 +15,41 @@ const knex = require('knex')({
 
 cut();
 
-async function cut(){
+async function cut() {
 
-    let tasks=await knex("t_cut").where({done:false})
+    let tasks = await knex("t_cut").where({done: false})
 
-    for(let task of tasks){
-        let rec=await knex("t_records").where({id:task.fileid})
-        let params =[
+    for (let task of tasks) {
+        let rec = await knex("t_records").where({id: task.fileid})
+        let params = [
             "-ss", task.markin,
-            "-i", "/var/mnt/spief2023/"+rec[0].filename,
-            "-c","copy",
+            "-i", "/var/mnt/spief2023/" + rec[0].filename,
+            "-c", "copy",
             "-movflags", "faststart",
             "-y",
-            "/var/mnt/spief2023/"+task.newfilename
+            "/var/mnt/spief2023/" + task.newfilename
 
         ]
-        await knex("t_cut").update({done:true}).where({id:task.id})
-        await knex("t_records").insert({
-            filename:task.newfilename,
-            done:true,
-            doneDate:new Date(),
-            trid:rec[0].trid,
-            lang:rec[0].lang,
-        })
+        let ffmpeg = spawn("ffmpeg", params);
+        ffmpeg.stdout.on('data', function (chunk) {
+            var textChunk = chunk.toString('utf8');
+            console.log(textChunk);
+        });
+        ffmpeg.stderr.on('data', function (chunk) {
+            var textChunk = chunk.toString('utf8');
+            console.error(textChunk);
+        });
+        ffmpeg.on('exit', async function () {
+            await knex("t_cut").update({done: true}).where({id: task.id})
+            await knex("t_records").insert({
+                filename: task.newfilename,
+                done: true,
+                doneDate: new Date(),
+                trid: rec[0].trid,
+                lang: rec[0].lang,
+            })
+        });
     }
 
-    setTimeout(cut, 10*1000);
+    setTimeout(cut, 10 * 1000);
 }
